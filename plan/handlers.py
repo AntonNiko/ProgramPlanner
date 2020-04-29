bd187ac34bd7a6fe540dbe605736bc0fb0a3a08e
Anton Nikitenko <ant.nikitenko@gmail.com>
2020-04-28 19:21:35 -0700

    Fixed issue with unrecognized Decimal128 representation

--- plan/handlers.py
+++ plan/handlers.py
@@ -1,5 +1,48 @@
 from django.contrib.auth import authenticate, login, logout
-from plan.models import CourseCode
+from plan.models import Course
+
+class DataHandler():
+
+    @staticmethod
+    def get_course_data(request):
+        """
+        Returns a list of courses corresponding to request parameters 
+        provided.
+
+        The following parameters specified by the `request` are supported:
+          - 'subject': If set, the result shall only contain courses under the subject
+          - 'subjects_only': If set, ignores all other parameters, and returns a list of all subjects
+          - 'number': If set, the result shall only contain courses that match the number
+          - 'numbers_only': If set, assumes that 'subject' is set, ignores all other parameters
+          ....
+
+        Args:
+          - request: An `HttpRequest` object.
+        Returns:
+          - response: A JSON-serializable object with result.
+        """
+
+        subject = request.GET.get('subject', None)
+        subjects_only = request.GET.get('subjects_only', None)
+        number = request.GET.get('number', None)
+        numders_only = request.GET.get('numbers_only', None)
+        number_min = request.GET.get('number_min', None)
+        number_max = request.GET.get('number_max', None)
+
+        return Course.objects.all()
+
+
+    @staticmethod
+    def get_program_data(request):
+        """
+        Returns a list of programs as specified by the request parameters provided.
+
+        Args:
+          - request: An `HttpRequest` object.
+        Returns:
+          - response: A JSON-serializable object with result.
+        """
+        
 
 class AccountHandler():
     """
@@ -39,45 +82,3 @@ class AccountHandler():
         logout(request)
 
 
-class DataHandler():
-
-    @staticmethod
-    def get_course_data(request):
-        """
-        Returns a list of courses corresponding to request parameters 
-        provided.
-
-        The following parameters specified by the `request` are supported:
-          - 'subject': If set, the result shall only contain courses under the subject
-          - 'subjects_only': If set, ignores all other parameters, and returns a list of all subjects
-          - 'number': If set, the result shall only contain courses that match the number
-          - 'numbers_only': If set, assumes that 'subject' is set, ignores all other parameters
-          ....
-
-        Args:
-          - request: An `HttpRequest` object.
-        Returns:
-          - response: A JSON-serializable object with result.
-        """
-
-        subject = request.GET.get('subject', None)
-        subjects_only = request.GET.get('subjects_only', None)
-        number = request.GET.get('number', None)
-        numders_only = request.GET.get('numbers_only', None)
-        number_min = request.GET.get('number_min', None)
-        number_max = request.GET.get('number_max', None)
-
-        #return Course.objects.all()
-
-
-    @staticmethod
-    def get_program_data(request):
-        """
-        Returns a list of programs as specified by the request parameters provided.
-
-        Args:
-          - request: An `HttpRequest` object.
-        Returns:
-          - response: A JSON-serializable object with result.
-        """
-        
--- plan/models/course_models.py
+++ plan/models/course_models.py
@@ -1,19 +1,20 @@
 from djongo import models
+from .fields import MongoDecimalField
 from .expression_models import Expression
 from .section_models import Section
 from .utils_models import CourseCode
 
 class UVicCourseDetails(models.Model):
-    hours_lectures = models.DecimalField(max_digits=4, decimal_places=2)
-    hours_labs = models.DecimalField(max_digits=4, decimal_places=2)
-    hours_tutorials = models.DecimalField(max_digits=4, decimal_places=2)
+    hours_lectures = MongoDecimalField(max_digits=4, decimal_places=2)
+    hours_labs = MongoDecimalField(max_digits=4, decimal_places=2)
+    hours_tutorials = MongoDecimalField(max_digits=4, decimal_places=2)
 
 class Course(models.Model):
     course_code = models.EmbeddedField(
         model_container = CourseCode
     )
     name = models.CharField(max_length=200)
-    credits = models.DecimalField(max_digits=3, decimal_places=1)
+    credits = MongoDecimalField(max_digits=3, decimal_places=1)
     course_details = models.EmbeddedField(
         model_container = UVicCourseDetails
     )
