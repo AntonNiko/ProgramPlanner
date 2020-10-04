26877838d79727d35d073e7974c33b0d26c07823
Anton Nikitenko <ant.nikitenko@gmail.com>
2020-10-04 14:17:42 -0700

    Registered models to view in admin page

--- plan/admin.py
+++ plan/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Course
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Course)
+admin.site.register(CourseOffering)
+admin.site.register(Section)
+admin.site.register(Meeting)
+
+admin.site.register(Schedule)
+admin.site.register(ScheduleSection)
+
+admin.site.register(Sequence)
+admin.site.register(Term)
+admin.site.register(TermCourse)
